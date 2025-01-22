29b6ef44b9253c145e44fb2578913c7ca9c2afb9
AwesomeOwl2007 <katemmveenstra@gmail.com>
2025-01-22 14:17:54 -0800

    Delete doc directory

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Driver"},{"p":"<Unnamed>","l":"Fraction"},{"p":"<Unnamed>","l":"FractionGame"},{"p":"<Unnamed>","l":"Pi"}];updateSearchResults();
